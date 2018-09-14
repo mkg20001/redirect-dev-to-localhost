0eb4c8002ff89885a65d779bb0ce9841c5824c91
Maciej Krüger <mkg20001@gmail.com>
2018-09-14 21:14:12 +0200

    chore: Tidy code

--- background.js
+++ background.js
@@ -1,26 +1,10 @@
 'use strict'
 
-/* const PROTO_RE = 'https?://'
-const DOMAIN_RE = '((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\\.(xn--)?([a-z0-9\\-]{1,61}|[a-z0-9-]{1,30}\\.[a-z]{2,})'
-
-const DEV_TLD = 'dev'
-const LOCAL_TLD = 'localhost'
-
-const RE = new RegExp('^' + PROTO_RE + DOMAIN_RE + '\\.' + DEV_TLD + '\\/.+', 'i')
-
-function listener (details) {
-  const m = details.url.match(RE)
-  if (m) {
-    return {redirect: 'http://' + RE[1].replace('.dev')}
-  }
-}  */
+/* eslint-env webextensions */
 
 function listener (details) {
   const {hostname, pathname} = new URL(details.url)
-  console.log(hostname, pathname)
-  if (hostname.endsWith('.dev')) {
-    return {redirectUrl: 'http://' + hostname.replace(/\.dev$/, '.localhost') + pathname}
-  }
+  return {redirectUrl: 'http://' + hostname.replace(/\.dev$/, '.localhost') + pathname}
 }
 
 browser.webRequest.onBeforeRequest.addListener(
